70bccc4c9882f5bc43c465092d5f3e99024a49a8
Christopher <chautekeur@dawan.fr>
2025-07-08 10:44:31 +0200

    Ajout commentaire main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tools import files_utils
 def main():
     files = files_utils.get_files_list()
     
+    # Parcours les fichiers
     for file in files:
         files_utils.download(file[0], file[1])
 
